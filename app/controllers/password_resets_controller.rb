80df1c77da01ad3fbd58c3bda22cb6f38c50fd0a
tuanvuP <tuanvu.workmail@gmail.com>
2019-06-17 09:28:41 +0700

    Chapter 12: Password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,58 @@
+class PasswordResetsController < ApplicationController
+  before_action :load_user, :valid_user, :check_expiration, only: [:edit, :update]
+
+  def new; end
+
+  def create
+    @user = User.find_by email: params[:password_reset][:email].downcase
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = t "password_reset_email_sent"
+      redirect_to root_url
+    else
+      flash.now[:danger] = t "email_not_found"
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add :password, t("cant_be_empty")
+      render :edit
+    elsif @user.update_attributes user_params
+      log_in @user
+      flash[:success] = t "password_reset_successfully"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def load_user
+    @user = User.find_by email: params[:email]
+    return if @user
+    render file: "public/404.html", status: :not_found, layout: false
+  end
+
+  def valid_user
+    unless @user&.activated? &&
+           @user.authenticated?(:reset, params[:id])
+      redirect_to root_url
+    end
+  end
+
+  def check_expiration
+    return unless @user.password_reset_expired?
+    flash[:danger] = t "expired_password"
+    redirect_to new_password_reset_url
+  end
+end
